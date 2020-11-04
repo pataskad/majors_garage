26c9d5e082bcfbdd1ecb5f05ebc4b691cd0849ae
Dallas Pataska <pataskad@gmail.com>
2020-11-04 12:11:34 -0600

    pre migration models initial

--- login/views.py
+++ login/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
