9660fcc57a26a8d51549ab75550306cd0566bd22
Dallas Pataska <pataskad@gmail.com>
2020-11-09 15:27:49 -0600

    11/9

--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.contrib import messages
 from .models import *
 
@@ -59,3 +60,4 @@ def elite_products(request):
 
 def coming_soon(request):
     return render(request, 'coming-soon.html')
+
